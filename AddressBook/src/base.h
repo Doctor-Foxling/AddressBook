549df2dedbf98402117aa59279566f3607106c14
Hamza Khan <30374624+Doctor-Foxling@users.noreply.github.com>
2022-09-22 09:27:55 +0100

    Update base.h

--- AddressBook/src/base.h
+++ AddressBook/src/base.h
@@ -2,23 +2,21 @@
 
 #include <pch.h>
 
-namespace Foundry {
 
-	template <typename T>
-	using Uniq = std::unique_ptr<T>;
+template <typename T>
+using Uniq = std::unique_ptr<T>;
 
-	template<typename T, typename ... Args>
-	constexpr Uniq<T> createUniq(Args&& ... args)
-	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
-	}
+template<typename T, typename ... Args>
+constexpr Uniq<T> createUniq(Args&& ... args)
+{
+	return std::make_unique<T>(std::forward<Args>(args)...);
+}
 
-	template <typename T>
-	using Shared = std::shared_ptr<T>;
+template <typename T>
+using Shared = std::shared_ptr<T>;
 
-	template <typename T, typename ... Args>
-	constexpr Shared<T> createShared(Args&& ... args)
-	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
-	}
+template <typename T, typename ... Args>
+constexpr Shared<T> createShared(Args&& ... args)
+{
+	return std::make_shared<T>(std::forward<Args>(args)...);
 }
