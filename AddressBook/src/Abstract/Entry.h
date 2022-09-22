bbcea322187b875c0a07c6dfbb3946aacc50cfa5
Hamza Khan <30374624+Doctor-Foxling@users.noreply.github.com>
2022-09-22 09:33:22 +0100

    Update Entry.h

--- AddressBook/src/Abstract/Entry.h
+++ AddressBook/src/Abstract/Entry.h
@@ -1,19 +1,15 @@
 #pragma once
 
-namespace Foundry {
+class Entry {
+public:
+	virtual ~Entry() = default;
 
-	class Entry {
-	public:
-		virtual ~Entry() = default;
-
-		virtual void setFirstName(const char* fName) = 0;
-		virtual void setLastName(const char* lName) = 0;
-		virtual const char* getFirstName() = 0;
-		virtual const char* getLastName() = 0;
-		virtual int getID() = 0;
-		virtual bool hasPhoneNumber() = 0;
-		virtual const char* getPhoneNumber() = 0;
-		virtual void addPhoneNumber(const char* ph) = 0;
-	};
-
-}
+	virtual void setFirstName(const char* fName) = 0;
+	virtual void setLastName(const char* lName) = 0;
+	virtual const char* getFirstName() = 0;
+	virtual const char* getLastName() = 0;
+	virtual int getID() = 0;
+	virtual bool hasPhoneNumber() = 0;
+	virtual const char* getPhoneNumber() = 0;
+	virtual void addPhoneNumber(const char* ph) = 0;
+};
