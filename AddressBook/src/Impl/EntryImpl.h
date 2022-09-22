bab8f64bd831aedd608143931227f0f70e69a10a
Hamza Khan <30374624+Doctor-Foxling@users.noreply.github.com>
2022-09-22 09:29:26 +0100

    Update EntryImpl.h

--- AddressBook/src/Impl/EntryImpl.h
+++ AddressBook/src/Impl/EntryImpl.h
@@ -2,51 +2,50 @@
 
 #include <Abstract/Entry.h>
 
-namespace Foundry {
 
-	class EntryImpl : public Entry
+class EntryImpl : public Entry
+{
+public:
+	EntryImpl() {};
+	EntryImpl(const char* fName, const char* lName): mFName(fName) ,mLName(lName)
+	{};
+	EntryImpl(const char* fName, const char* lName, const char* phNum)
+		: mFName(fName), mLName(lName), mPhoneNumber(phNum) {};
+
+	virtual ~EntryImpl() {};
+
+	virtual void setFirstName(const char* fName) override { mFName = fName; }
+	virtual void setLastName(const char* lName) override { mLName = lName; }
+
+	virtual const char* getFirstName() override { return mFName; }
+	virtual const char* getLastName() override { return mLName; }
+
+	virtual int getID() override { return ID; }
+
+	static Shared<Entry> Create();
+	static Shared<Entry> Create(const char* fName, const char* lName);
+	static Shared<Entry> Create(const char* fName, const char* lName, const char* phoneNumber);
+
+	virtual bool hasPhoneNumber() { return mPhoneNumber != '\0'; }
+
+	virtual const char* getPhoneNumber() override { return mPhoneNumber; }
+
+	virtual void addPhoneNumber(const char* ph) override { mPhoneNumber = ph; }
+
+	bool operator==(Shared<Entry> other)
 	{
-	public:
-		EntryImpl() {};
-		EntryImpl(const char* fName, const char* lName): mFName(fName) ,mLName(lName)
-		{};
-		EntryImpl(const char* fName, const char* lName, const char* phNum)
-			: mFName(fName), mLName(lName), mPhoneNumber(phNum) {};
-		
-		virtual ~EntryImpl() {};
-
-		virtual void setFirstName(const char* fName) override { mFName = fName; }
-		virtual void setLastName(const char* lName) override { mLName = lName; }
-
-		virtual const char* getFirstName() override { return mFName; }
-		virtual const char* getLastName() override { return mLName; }
-
-		virtual int getID() override { return ID; }
-
-		static Shared<Entry> Create();
-		static Shared<Entry> Create(const char* fName, const char* lName);
-		static Shared<Entry> Create(const char* fName, const char* lName, const char* phoneNumber);
-
-		virtual bool hasPhoneNumber() { return mPhoneNumber != '\0'; }
-
-		virtual const char* getPhoneNumber() override { return mPhoneNumber; }
-
-		virtual void addPhoneNumber(const char* ph) override { mPhoneNumber = ph; }
-
-		bool operator==(Shared<Entry> other)
-		{
-			// Should have implemented an ID system but names will have to do for now
-			return (mFName == other->getFirstName() 
-				&& mLName == other->getLastName() && ID == other->getID());
-		}
+		// Should have implemented an ID system but names will have to do for now
+		return (mFName == other->getFirstName() 
+			&& mLName == other->getLastName() && ID == other->getID());
+	}
 
 
- 
-	private:
-		const char* mFName;
-		const char* mLName;
-		const char* mPhoneNumber = '\0';
-		int ID;
-	};
 
-}
+private:
+	const char* mFName;
+	const char* mLName;
+	const char* mPhoneNumber = '\0';
+	int ID;
+};
+
+
