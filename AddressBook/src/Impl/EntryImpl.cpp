c0452b4a633c7cb8168d67eea0a9285c077fd3d7
Hamza Khan <30374624+Doctor-Foxling@users.noreply.github.com>
2022-09-22 09:29:52 +0100

    Update EntryImpl.cpp

--- AddressBook/src/Impl/EntryImpl.cpp
+++ AddressBook/src/Impl/EntryImpl.cpp
@@ -2,22 +2,19 @@
 #include "base.h"
 #include "EntryImpl.h"
 
-namespace Foundry
+
+Shared<Entry> EntryImpl::Create()
 {
-    Shared<Entry> EntryImpl::Create()
-    {
-        // In future, other versions of an Entry can be returned based on some condition
-        return createShared<EntryImpl>();
-    }
-    Shared<Entry> EntryImpl::Create(const char* fName, const char* lName)
-    {
-        return createShared<EntryImpl>(fName, lName);
-    }
-
-    Shared<Entry> EntryImpl::Create(const char* fName, const char* lName, const char* phoneNumber)
-    {
-        return createShared<EntryImpl>(fName, lName, phoneNumber);
-    }
-
-
+    // In future, other versions of an Entry can be returned based on some condition
+    return createShared<EntryImpl>();
 }
+Shared<Entry> EntryImpl::Create(const char* fName, const char* lName)
+{
+    return createShared<EntryImpl>(fName, lName);
+}
+
+Shared<Entry> EntryImpl::Create(const char* fName, const char* lName, const char* phoneNumber)
+{
+    return createShared<EntryImpl>(fName, lName, phoneNumber);
+}
+
